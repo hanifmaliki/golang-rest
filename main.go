2161060bc16aa8b412ee3ae5007bf91c7f2a14e2
Hanif Maliki <hanifmaliki97@gmail.com>
2022-06-02 07:58:39 +0700

    Initial

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"Desc"`
+	Content string `json:"Content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title1", Desc: "Test Desctiption1", Content: "Hello World1"},
+		Article{Title: "Test Title2", Desc: "Test Desctiption2", Content: "Hello World2"},
+	}
+
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	handleRequest()
+}
